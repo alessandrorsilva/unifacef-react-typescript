ba07b3a88f26c29797e870fe8a8065ff34a84a5e
Alessandro Rodrigues da Silva <alessandrosilva.ti@gmail.com>
2022-04-09 11:15:45 -0300

    Aula 03 git 5

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -1,6 +1,7 @@
 import Home from "../containers/home";
 import { RouteProps } from "react-router-dom";
 import Combustivel from "../containers/combustivel";
+import StarWars from "../containers/star-wars";
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -12,4 +13,6 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/`, component: Home, exact: true },
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
+  { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
+  { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
 ];

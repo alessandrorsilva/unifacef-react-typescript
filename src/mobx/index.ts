e8eb837dd9287e13523baf0e2849ba4b899b7b58
Alessandro Rodrigues da Silva <alessandrosilva.ti@gmail.com>
2022-04-09 11:26:40 -0300

    Aula 03 git 6

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -2,10 +2,12 @@ import { home } from '../containers/home/store';
 import { combustivel } from '../containers/combustivel/store';
 import { router } from './router.store';
 import { starWars } from '../containers/star-wars/store';
+import { starWarsDetails } from '../containers/star-wars-details/store';
 
 export {
   router,
   combustivel,
   home,
   starWars,
+  starWarsDetails,
 }
--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -2,6 +2,7 @@ import Home from "../containers/home";
 import { RouteProps } from "react-router-dom";
 import Combustivel from "../containers/combustivel";
 import StarWars from "../containers/star-wars";
+import StarWarsDetails from "../containers/star-wars-details";
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -13,6 +14,6 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/`, component: Home, exact: true },
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
-  { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
   { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
+  { path: `${publicUrl}/star-wars-details`,  name: 'Star Wars Details', component: StarWarsDetails, exact: true },
 ];

135b1bf504a1f1fc44e8a80cfe9ef32e583e030f
Alessandro Rodrigues da Silva <alessandrosilva.ti@gmail.com>
2022-04-09 10:58:28 -0300

    Aula 03 git 4

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -1,9 +1,11 @@
 import { home } from '../containers/home/store';
 import { combustivel } from '../containers/combustivel/store';
 import { router } from './router.store';
+import { starWars } from '../containers/star-wars/store';
 
 export {
   router,
   combustivel,
-  home
+  home,
+  starWars,
 }
